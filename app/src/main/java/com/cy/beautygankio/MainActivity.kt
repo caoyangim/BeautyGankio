5e822de7865616fa942a0d7a08b18ac22a7d82e1
caoyangim <caoyangim@163.com>
2020-11-26 00:16:32 +0800

    新增侧滑菜单功能。

--- app/src/main/java/com/cy/beautygankio/MainActivity.kt
+++ app/src/main/java/com/cy/beautygankio/MainActivity.kt
@@ -2,12 +2,19 @@ package com.cy.beautygankio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.cy.beautygankio.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
--- app/src/main/java/com/cy/beautygankio/ui/home/GankFragment.kt
+++ app/src/main/java/com/cy/beautygankio/ui/home/GankFragment.kt
@@ -1,14 +1,16 @@
 package com.cy.beautygankio.ui.home
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.cy.beautygankio.MainActivity
 import com.cy.beautygankio.R
 import com.cy.beautygankio.databinding.FragmentGankBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -24,7 +26,9 @@ class GankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentGankBinding.inflate(inflater,container,false)
+        binding = FragmentGankBinding.inflate(inflater, container, false)
+
+        linkageToolbarAndDrawerWithAnim()
 
         setFragmentResultListener(getString(R.string.fragment_request_key)) { key, bundle ->
             binding.appbarLayout.setExpanded(false)
@@ -34,12 +38,31 @@ class GankFragment : Fragment() {
         val viewPager = binding.viewPager
 
         viewPager.adapter = GankPagerAdapter(this)
-        TabLayoutMediator(tabLayout,viewPager) { tab, position ->
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = getTabTitle(position)
 //            tab.setIcon(getTabIcon(position))
         }.attach()
 
         return binding.root
+    }
+
+    //设置导航栏图标
+    private fun linkageToolbarAndDrawer() {
+        (activity as MainActivity).also {
+            val navController = it.findNavController(R.id.nav_host)
+            val appBarConfiguration = AppBarConfiguration(navController.graph,binding.drawerLayout)
+            binding.toolbar.setupWithNavController(navController,appBarConfiguration)
+        }
+    }
+
+    //设置带动画的导航栏图标
+    private fun linkageToolbarAndDrawerWithAnim() {
+        val toggle = ActionBarDrawerToggle(
+            activity,binding.drawerLayout,binding.toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        //设置左上角显示三道横线
+        toggle.syncState()
     }
 
     private fun getTabIcon(position: Int): Int {
